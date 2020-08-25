837f45a19e4597a5d9d01e33f71be8230ed785ac
Terry Smith <tersmi1@gmail.com>
2020-08-25 07:48:49 -0400

    Getting there more

---
+++ app/src/main/java/com/example/ngbp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ngbp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.GridView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // var gridView = findViewById(R.id.gridlayout) as GridView
+    }
+}
