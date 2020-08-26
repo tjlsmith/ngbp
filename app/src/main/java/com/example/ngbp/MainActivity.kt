e10bb0ccf0e645a1077af25d7c2b095677ac28fc
Terry Smith <tersmi1@gmail.com>
2020-08-25 20:59:47 -0400

    Added tags to all computer buttons

--- app/src/main/java/com/example/ngbp/MainActivity.kt
+++ app/src/main/java/com/example/ngbp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.ngbp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.GridView
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.view.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,4 +16,13 @@ class MainActivity : AppCompatActivity() {
 
         // var gridView = findViewById(R.id.gridlayout) as GridView
     }
+
+    fun kaBoom(v: View?) {
+        // var button = findViewById<Button>(R.id.NGBP.)
+        val imgBtn = findViewById(v!!.id) as ImageButton
+        //val col = imgBtn.name
+        //val row = imgBtn.NGBP.rowCount
+
+    }
+
 }
