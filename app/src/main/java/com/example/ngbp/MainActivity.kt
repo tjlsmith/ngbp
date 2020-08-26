39beb9a7fff93d52e306ba66264b4baed5d38dd2
Terry Smith <tersmi1@gmail.com>
2020-08-26 11:19:45 -0400

    get row and column from the tag

--- app/src/main/java/com/example/ngbp/MainActivity.kt
+++ app/src/main/java/com/example/ngbp/MainActivity.kt
@@ -2,10 +2,8 @@ package com.example.ngbp
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +18,17 @@ class MainActivity : AppCompatActivity() {
     fun kaBoom(v: View?) {
         // var button = findViewById<Button>(R.id.NGBP.)
         val imgBtn = findViewById(v!!.id) as ImageButton
+        val tag =  Integer.parseInt(v.getTag().toString()) //.toInt()
+        var row = -1
+        var col = -1
+        if (tag < 10) {
+           row = 0
+            col=tag
+        } else {
+            row=tag/10
+            col=tag % 10
+        }
+        val dummy = 1
         //val col = imgBtn.name
         //val row = imgBtn.NGBP.rowCount
 
