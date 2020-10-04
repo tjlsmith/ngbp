37ddbff23655b34bbd637a03caf0bd964f850eb9
Terry Smith <tersmi1@gmail.com>
2020-10-04 13:54:11 -0400

    New Activity Title Page working!

--- app/src/main/java/com/example/ngbp/TitleActivity2.kt
+++ app/src/main/java/com/example/ngbp/TitleActivity2.kt
@@ -1,11 +1,19 @@
 package com.example.ngbp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_title2.*
 
 class TitleActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_title2)
+
+        TitleActivityLayout.setOnClickListener() {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
     }
 }
