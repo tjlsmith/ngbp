d96cad9565f51a364c6cc8dd9273401ac8c8b498
Terry Smith <tersmi1@gmail.com>
2020-10-03 21:48:18 -0400

    New Activity Title Page

---
+++ app/src/main/java/com/example/ngbp/TitleActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.ngbp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TitleActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_title2)
+    }
+}
